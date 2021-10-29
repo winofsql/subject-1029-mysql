8d8b368f41a8f6a7f4e593c753485aaea97849dc
sworc <lightbox@nifty.com>
2021-10-29 12:29:26 +0900

    Add files via upload

--- 003.sql
+++ 003.sql
@@ -1,5 +1,23 @@
 LOAD DATA INFILE 'C:/app/workspace/subject-1029/sql-1029-01/syain.csv' 
 INTO TABLE lightbox.社員マスタ
+CHARACTER SET cp932
  FIELDS TERMINATED BY ','
  --    OPTIONALLY ENCLOSED BY '"'
  LINES TERMINATED BY '\r\n';
+
+
+load data infile 'C:\\app\\workspace\\subject-1029\\sql-1029-01\\syain.csv'
+  replace  
+into table `社員マスタ`
+CHARACTER SET cp932
+ fields terminated by ','
+ lines terminated by '\r\n';
+
+load data infile 'C:\\app\\workspace\\subject-1029\\sql-1029-01\\syain.csv'
+ ignore
+into table `社員マスタ`
+CHARACTER SET cp932
+ fields terminated by ','
+ lines terminated by '\r\n';
+
+SHOW CHARACTER SET;
