cf93700a6f941386d27acc286727ad7bfd964c61
sworc <lightbox@nifty.com>
2021-10-29 12:07:28 +0900

    Add files via upload

--- 002.sql
+++ 002.sql
@@ -6,8 +6,27 @@ SELECT
 FROM
     `社員マスタ`;
 
-
+CREATE TABLE `社員マスタB` AS
 SELECT
     *
 FROM
-    社員マスタ;
+    社員マスタ
+WHERE
+    社員コード BETWEEN '0010'
+    AND '0020';
+
+ALTER TABLE
+    社員マスタa
+ADD
+    PRIMARY KEY(社員コード);
+
+ALTER TABLE
+    社員マスタa DROP PRIMARY KEY;
+
+CREATE UNIQUE INDEX SYAIN01 ON 社員マスタa (社員コード);
+
+DROP INDEX SYAIN01 ON 社員マスタa;
+
+CREATE
+--    UNIQUE
+INDEX SYAIN_NAME ON 社員マスタa (氏名);
